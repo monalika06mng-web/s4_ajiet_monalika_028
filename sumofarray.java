da57bb3af36bf471b9637d07230c10a099716d38
monalika06mng-web <monalika06mng@gmail.com>
2026-05-08 14:47:41 +0530

    Create sumofarray.java
    sum of the given array

---
+++ sumofarray.java
@@ -0,0 +1,10 @@
+class sum{
+public static void main (String[] args){
+    int []a={1,2,3,4,5};
+    int s=0;
+    for(int i=0;i<a.length;i++){
+        s+=a[i];
+        System.out.println("sum"+s);
+    }
+    }
+} 
